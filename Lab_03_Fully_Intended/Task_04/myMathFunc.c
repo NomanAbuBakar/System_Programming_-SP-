6542f20d02ddf5bd5f85c63ab38c054f638134bd
NomanAbuBakar <36780231+NomanAbuBakar@users.noreply.github.com>
2018-03-17 01:33:53 +0500

    Add files via upload

---
+++ Lab_03_Fully_Intended/Task_04/myMathFunc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+// Check for 2 Values to be Equal or Not...
+int isEqual(int first_number, int second_number)
+{
+	if(first_number == second_number)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+// Swapping Without Using 3rd Variable ...
+void swap(int *first_number, int *second_number)
+{
+	printf("The Definition has been Changed!");
+	*first_number = *first_number + *second_number;
+	*second_number = *first_number - *second_number;
+	*first_number = *first_number - *second_number;
+}
+
